802cff4945e372981b47b6abafaefa5a625f7850
xdd1874 <bepox0531@gmail.com>
2018-07-31 22:18:22 +0800

    .

---
+++ src/com/study/Main.java
@@ -0,0 +1,11 @@
+package com.study;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(new Date());
+    }
+}
