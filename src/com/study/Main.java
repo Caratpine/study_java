cf8a17de6f99f2a87d4b389f16ce1af67823c18a
xdd1874 <bepox0531@gmail.com>
2018-07-31 22:23:00 +0800

    .

--- src/com/study/Main.java
+++ src/com/study/Main.java
@@ -6,6 +6,18 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println(new Date());
+        Random rand = new Random(47);
+        float f[] = new float[10];
+        for(int i = 0; i < 10; i++) {
+            f[i] = rand.nextFloat();
+        }
+
+        for (float x : f) {
+            System.out.println(x);
+        }
+
+        for(char c : "An African Swallow.".toCharArray()) {
+            System.out.print(c + " ");
+        }
     }
 }
