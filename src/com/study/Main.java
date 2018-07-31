d4a03738ef28d326204ba2a68b7cbc9bcb892953
xdd1874 <bepox0531@gmail.com>
2018-07-31 22:48:33 +0800

    .

--- src/com/study/Main.java
+++ src/com/study/Main.java
@@ -23,11 +23,42 @@ public class Main {
         for(int i = 0; i < 10; i++) {
             new Rock();
         }
+
+        for(int i = 0; i < 5; i++) {
+            Tree t = new Tree(i);
+            t.info();
+            t.info("overloaded method");
+        }
+
+        new Tree();
     }
 }
 
 class Rock {
     Rock() {
         System.out.print("Rock ");
+    }
+}
+
+class Tree {
+    int height;
+
+    Tree() {
+        System.out.println("Planting a seedling");
+        height = 0;
+    }
+
+    Tree(int initialHeight) {
+        height = initialHeight;
+        System.out.print("Create new Tree that is " +
+                height + " feet tall");
+    }
+
+    void info() {
+        System.out.println("Tree is " + height + " feet tall");
+    }
+
+    void info(String s) {
+        System.out.println(s + ": Tree is " + height + " feet tall");
     }
 }
