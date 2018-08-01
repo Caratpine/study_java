0f22f591409172bcb3b66513f0305e40f2b93cb8
xdd1874 <bepox0531@gmail.com>
2018-08-01 21:13:27 +0800

    .

---
+++ src/com/study/PassingThis.java
@@ -0,0 +1,28 @@
+package com.study;
+
+public class PassingThis {
+    public static void main(String[] args) {
+        new Person().eat(new Apple());
+    }
+
+}
+
+
+class Person {
+    public void eat(Apple apple) {
+        Apple peeled = apple.getPeeled();
+        System.out.println("Yummy");
+    }
+}
+
+class Apple {
+    Apple getPeeled() {
+        return Peeler.peel(this);
+    }
+}
+
+class Peeler {
+    static Apple peel(Apple apple) {
+        return apple;
+    }
+}
