ad742d9cbdec55a150a743fc63cdcd03bf5f8141
xdd1874 <bepox0531@gmail.com>
2018-08-02 16:31:58 +0800

    .

---
+++ src/com/study/Dinner.java
@@ -0,0 +1,9 @@
+package com.study;
+
+import com.study.dessert.Cookie;
+
+public class Dinner {
+    public static void main(String[] args) {
+        Cookie x = new Cookie();
+    }
+}
---
+++ src/com/study/dessert/Cookie.java
@@ -0,0 +1,13 @@
+package com.study.dessert;
+
+import static com.study.print.Print.*;
+
+public class Cookie {
+    public Cookie() {
+        print("Cookie constructor");
+    }
+
+    void bite() {
+        print("bite");
+    }
+}
