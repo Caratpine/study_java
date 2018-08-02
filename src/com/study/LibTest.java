eaf10c03df28b55af1aeb6290f6b84ca56dc416f
xdd1874 <bepox0531@gmail.com>
2018-08-02 16:12:58 +0800

    .

---
+++ src/com/study/LibTest.java
@@ -0,0 +1,11 @@
+package com.study;
+
+import com.study.simple.Vector;
+import com.study.simple.List;
+
+public class LibTest {
+    public static void main(String[] args) {
+        Vector v = new Vector();
+        List l = new List();
+    }
+}
