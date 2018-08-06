a776bfbbde3d4b296a562b4e2b22f090c2a8e6ae
xdd1874 <bepox0531@gmail.com>
2018-08-06 18:08:05 +0800

    .

---
+++ src/com/study/music/Music4.java
@@ -0,0 +1,80 @@
+package com.study.music;
+
+import com.study.music.Note;
+
+import static com.study.print.Print.print;
+
+public class Music4 {
+}
+
+abstract class InstrumentA {
+    private int i;
+
+    public abstract void play(Note n);
+
+    public String what() {
+        return "Instrument";
+    }
+
+    public abstract void adjust();
+}
+
+class WindA extends InstrumentA {
+    public void play(Note n) {
+        print("Wind.play() " + n);
+    }
+
+    public String what() {
+        return "Wind";
+    }
+
+    public void adjust() {}
+}
+
+class PercussionA extends InstrumentA {
+    public void play(Note n) {
+        prinf("Percussion.play() " + n);
+    }
+
+    public String what() {
+        return "Percussion";
+    }
+
+    public void adjust() {
+
+    }
+}
+
+class Stringed extends InstrumentA {
+    public void play(Note n) {
+        print("Stringed.play() " + n);
+    }
+
+    public String what() {
+        return "Stringed";
+    }
+
+    public void adjust() {}
+
+}
+
+class Brass extends WindA {
+    public void play(Note n) {
+        print("Brass.play() "+ n);
+    }
+
+    public void adjust() {
+        print("Brass.adjust()");
+    }
+}
+
+class Woodwind extends Wind {
+    public void play(Note n) {
+        print("Woodwind.play() " + n);
+    }
+
+    public String what() {
+        return "Woodwind";
+    }
+}
+
