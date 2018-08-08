7dbaa252efcec55933fa3fccd4a7b215ac678ded
xdd1874 <bepox0531@gmail.com>
2018-08-08 21:57:22 +0800

    .

---
+++ src/com/study/strings/Concatenation.java
@@ -0,0 +1,11 @@
+package com.study.strings;
+
+import static com.study.print.Print.print;
+
+public class Concatenation {
+    public static void main(String[] args) {
+        String mango = "mango";
+        String s = "abc" + mango + "def" + 47;
+        print(s);
+    }
+}
