aaa2861b9f682c54ed0c13a9c02b3c348f675bbf
xdd1874 <bepox0531@gmail.com>
2018-08-10 21:56:19 +0800

    .

---
+++ src/com/study/typeinfo/Person.java
@@ -0,0 +1,7 @@
+package com.study.typeinfo;
+
+public class Person extends Individual {
+    public Person(String name) {
+        super(name);
+    }
+}
--- src/com/study/typeinfo/ToyTest.java
+++ src/com/study/typeinfo/ToyTest.java
@@ -1,10 +1,35 @@
 package com.study.typeinfo;
 
+import static com.study.print.Print.print;
+
 public class ToyTest {
+    public static void main(String[] args) {
+        Toy toy = new Toy();
+        toy.say();
+    }
 }
 
 class Toy {
     Toy() {}
     Toy(int i) {}
+
+    @Deprecated
+    public void say() {
+        print("hello world");
+    }
+
+    public void speak() {
+        print("aaa");
+    }
 }
 
+@interface TestAnnotation {
+    String value();
+}
+
+@TestAnnotation("hi")
+class Test extends Toy {
+
+    @Override
+    public void speak() {}
+}
