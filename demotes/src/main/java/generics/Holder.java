1f60392bc07bdae50210884f641f03706efe341a
xdd1874 <bepox0531@gmail.com>
2019-03-07 20:57:32 +0800

    .

---
+++ demotes/src/main/java/generics/Holder.java
@@ -0,0 +1,26 @@
+package generics;
+
+public class Holder<T> {
+
+    private T a;
+
+    public Holder(T a) {
+        this.a = a;
+    }
+
+    public void set(T a) {
+        this.a = a;
+    }
+
+    public T get() {
+        return this.a;
+    }
+
+    public static void main(String[] args) {
+        Holder<Automobile> h = new Holder<Automobile>(new Automobile());
+        Automobile a = h.get();
+    }
+}
+
+
+class Automobile {}
