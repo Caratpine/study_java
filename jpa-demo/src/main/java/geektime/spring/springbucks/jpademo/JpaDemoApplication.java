90343d0632f35c2d23c94e4a8b67f1aeda2bb0a9
xdd1874 <bepox0531@gmail.com>
2019-03-11 20:26:12 +0800

    .

---
+++ jpa-demo/src/main/java/geektime/spring/springbucks/jpademo/JpaDemoApplication.java
@@ -0,0 +1,13 @@
+package geektime.spring.springbucks.jpademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaDemoApplication.class, args);
+	}
+
+}
--- jpa-demo/src/main/java/geektime/spring/springbucks/jpademo/model/Coffee.java
+++ jpa-demo/src/main/java/geektime/spring/springbucks/jpademo/model/Coffee.java
@@ -1,4 +1,46 @@
 package geektime.spring.springbucks.jpademo.model;
 
-public class Coffee {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.Parameter;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.joda.money.Money;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "T_MENU")
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coffee implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @Column
+    @Type(type = "org.jadira.usertype.moneyandcurrency.joda.PersistentMoneyAmount",
+            parameters = {@Parameter(name = "currencyCode", value = "CNY")})
+    private Money price;
+
+    @Column(updatable = false)
+    @CreationTimestamp
+    private Date createTime;
+
+    @UpdateTimestamp
+    private Date updateTime;
 }
