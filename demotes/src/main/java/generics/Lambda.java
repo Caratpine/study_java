d06ad69716c6292480ee9d20376ae28925dac0cd
xdd1874 <bepox0531@gmail.com>
2019-03-18 20:48:42 +0800

    .

--- demotes/src/main/java/generics/Lambda.java
+++ demotes/src/main/java/generics/Lambda.java
@@ -2,11 +2,13 @@ package generics;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 public class Lambda {
     public static void main(String[] args) {
         Lambda lambda = new Lambda();
         lambda.oldWay();
+        lambda.newWay();
     }
 
     private void oldWay() {
@@ -18,5 +20,18 @@ public class Lambda {
             }
         });
         System.out.println(String.join(", ", s));
+    }
+
+    private void newWay() {
+        String[] s = "Improving code with Lambda expressions in Java 8".split(" ");
+        Arrays.sort(s, (s1, s2) -> {
+            return s1.toLowerCase().compareTo(s2.toLowerCase());
+        });
+        System.out.println(String.join(", ", s));
+
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        list.forEach(v -> {
+            System.out.println(v);
+        });
     }
 }
