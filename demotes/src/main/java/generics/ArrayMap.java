e88c9bd00787fcb25482ca95a8bf230c30a49025
xdd1874 <bepox0531@gmail.com>
2019-05-21 17:09:28 +0800

    .

---
+++ demotes/src/main/java/generics/ArrayMap.java
@@ -0,0 +1,42 @@
+package generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayMap<K, V> {
+
+    private List<K> keys = new ArrayList<>();
+    private List<V> values = new ArrayList<>();
+
+    public V put(K k, V v) {
+        for (int i = 0; i < keys.size(); i++) {
+            if (keys.get(i).equals(k)) {
+                V oldv = values.get(i);
+                values.set(i, v);
+                return oldv;
+            }
+        }
+        keys.add(k);
+        values.add(v);
+        return null;
+    }
+
+    public V get(K k) {
+        for (int i = 0; i < keys.size(); i++) {
+            if (keys.get(i).equals(k)) {
+                return values.get(i);
+            }
+        }
+        return null;
+    }
+
+    public V delete(K k) {
+        for (int i = 0; i < keys.size(); i++) {
+            if (keys.get(i).equals(k)) {
+                keys.remove(i);
+                return values.remove(i);
+            }
+        }
+        return null;
+    }
+}
